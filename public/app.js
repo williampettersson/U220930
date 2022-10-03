30d594a064358abfeaeb99c1f280602407b67d09
williampettersson <william.c.pettersson@gmail.com>
2022-10-03 11:04:28 +0200

    forgot to commit, added array and foreach loops

---
+++ public/app.js
@@ -0,0 +1,32 @@
+const structure = new XMLHttpRequest();
+structure.open('GET', 'structure.json');
+structure.send();
+
+const carsArray = new XMLHttpRequest();
+carsArray.open('GET', 'cars.json');
+carsArray.send();
+
+const keys = JSON.parse(structure.responseText);
+
+
+carList.onload = () => {
+    const cars = JSON.parse(carsArray.responseText);
+
+    var list = document.createElement('ul');
+    carList.appendChild(list);
+
+    cars.Cars.forEach(anka => {
+        console.log(anka);
+        var listRow = document.createElement('li');
+        listRow.innerHTML = (`${anka.Brand}, ${anka.Model}, ${anka.Year}`);
+
+        list.appendChild(listRow);
+
+    });
+}
+
+
+
+loadCars = () => {
+    carList.hidden = false;
+}
